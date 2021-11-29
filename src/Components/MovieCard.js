2dd0cf4ecf7ee26a4b254af5549f9ab1553234fb
MarwenKing15 <marweng.touati@gmail.com>
2021-11-29 09:49:49 +0100

    Final Changes

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,31 +1,36 @@
-import React from 'react';
+import React from "react";
 
-import {Card} from 'react-bootstrap';
-import Rating from '@mui/material/Rating';
+import { Card } from "react-bootstrap";
+import Rating from "@mui/material/Rating";
 
-import './Styles/MovieCard.css';
-
-
-
-
+import "./Styles/MovieCard.css";
 
 function MovieCard(props) {
-    return (
-        <div className="cont">
-		<div class="main-container">
-              <img src={props.movie.posterURL} alt="movie poster" className="poster" width="320px"/>
-              <Card.Body className="info-container">
-                <Card.Title className="crd__title">{props.movie.title}</Card.Title>
-                <Card.Text className="cardDesc">{props.movie.desc}</Card.Text>
-                <Card.Text className="cardDesc">{props.movie.year}</Card.Text>
-                <Rating name="read-only" value={props.movie.rating} readOnly className="cardDesc"/>
-                <br/>                
-              </Card.Body>
-            </div>
-            <br/>
-        </div>
-
-    )
+  return (
+    <div className="cont">
+      <div class="main-container">
+        <img
+          src={props.movie.posterURL}
+          alt="movie poster"
+          className="poster image-container"
+          width="320px"
+        />
+        <Card.Body className="info-container">
+          <Card.Title className="crd__title">{props.movie.title}</Card.Title>
+          <Card.Text className="cardDesc">{props.movie.desc}</Card.Text>
+          <Card.Text className="cardDesc">{props.movie.year}</Card.Text>
+          <Rating
+            name="read-only"
+            value={props.movie.rating}
+            readOnly
+            className="cardDesc"
+          />
+          <br />
+        </Card.Body>
+      </div>
+      <br />
+    </div>
+  );
 }
 
 export default MovieCard;
--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -1,39 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import { Container, Row, Col } from 'react-bootstrap';
-import MovieCard from './MovieCard';
+import { Container, Row, Col } from "react-bootstrap";
+import MovieCard from "./MovieCard";
 import MovieListHeading from "./MovieListHeading";
 
-import './Styles/MovieList.css';
+import "./Styles/MovieList.css";
 
-
-
-function MovieList({movies,searchValue,value}) {
-    return (
-        <div className="body">
-            <br/>
-            <MovieListHeading heading='Movies'/>
-            <br/>
-<Container>
-                <Row>
-          { movies
-          .filter((movie) =>
-              movie.title
-                .toLowerCase()
-                .includes(searchValue.toLowerCase()) && movie.rating>=value
+function MovieList({ movies, searchValue, value }) {
+  return (
+    <div className="body">
+      <br />
+      <MovieListHeading heading="Movies" />
+      <br />
+      <Container>
+        <Row>
+          {movies
+            .filter(
+              (movie) =>
+                movie.title.toLowerCase().includes(searchValue.toLowerCase()) &&
+                movie.rating >= value
             )
             .map((movie) => (
-            <Col >
+              <Col>
                 <MovieCard movie={movie} />
-            </Col>
-            ))
-            
-            }
-            </Row>
-</Container>
-            
-        </div>
-    )
+              </Col>
+            ))}
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
-export default MovieList
+export default MovieList;
